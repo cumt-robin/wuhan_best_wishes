b533832b6aec64967c6d2fe6cacc270ac4be7840
jiangwenbin <wenbin.jiang@coollu.com.cn>
2020-02-06 18:18:01 +0800

    feat: 支持省份查询

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,19 @@ var fs = require('fs');
 var router = express.Router();
 
 router.get('/timeline', function(req, res, next) {
-  fs.createReadStream('./data/timeline.json').pipe(res)
+  fs.createReadStream('./data/timeline.json', {encoding: 'utf8'}).pipe(res)
 });
 
 router.get('/stats', function(req, res, next) {
-  fs.createReadStream('./data/stats.json').pipe(res)
+  fs.createReadStream('./data/stats.json', {encoding: 'utf8'}).pipe(res)
+});
+
+router.get('/province_stats', function(req, res, next) {
+  fs.readFile('./data/province_stats.json', (err, data) => {
+    if (err) throw err;
+    const jsonData = JSON.parse(data.toString())
+    res.json(jsonData)
+  });
 });
 
 module.exports = router;
